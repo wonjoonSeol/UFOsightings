2a46fd82113ff05325b173abf59102909097b8c5
Britton Forsyth <bforsyth927@gmail.com>
2017-03-20 19:19:18 +0000

    Added basic stat display functionality and panel switching.

--- ppa-major-coursework/src/assessment/view/SubStatPanel.java
+++ ppa-major-coursework/src/assessment/view/SubStatPanel.java
@@ -8,6 +8,9 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
+import assessment.controller.StatController;
+import assessment.model.Model;
+
 public class SubStatPanel extends JPanel {
 
 	private JButton rButton;
@@ -16,10 +19,14 @@ public class SubStatPanel extends JPanel {
 	private JLabel centLabel;
 	private JPanel lPanel;
 	private JPanel rPanel;
+	private Model model;
+	private int statNumber;
 
-	public SubStatPanel() {
+	public SubStatPanel(Model m) {
 		super();
 		setLayout(new BorderLayout());
+		model = m;
+		statNumber = 0;
 		initWidgets();
 	}
 
@@ -28,7 +35,9 @@ public class SubStatPanel extends JPanel {
 		rPanel = new JPanel(new BorderLayout());
 
 		rButton = new JButton(">");
+		rButton.addActionListener(new StatController(this));
 		lButton = new JButton("<");
+		lButton.addActionListener(new StatController(this));
 		lPanel.add(lButton, BorderLayout.CENTER);
 		rPanel.add(rButton, BorderLayout.CENTER);
 
@@ -40,5 +49,51 @@ public class SubStatPanel extends JPanel {
 		add(topLabel, BorderLayout.NORTH);
 		add(centLabel, BorderLayout.CENTER);
 	}
+
+	public void setStat(int i) {
+		
+		if(i < 1)
+		{
+			i = 4;
+		}
+		else if(i > 4)
+		{
+			i = 1;
+		}
+		
+		if (i == 1) 
+		{
+				statNumber = 1;
+				topLabel.setText("Hoax Stats");
+				centLabel.setText(Integer.toString(model.getNumHoaxes()));
+		} 
+		else if (i == 2) 
+		{
+				statNumber = 2;
+				topLabel.setText("Non-US Stats");
+				centLabel.setText(Integer.toString(model.getNonUSSight()));
+		} 
+		else if (i == 3) 
+		{
+				statNumber = 3;
+				topLabel.setText("Likeliest State");
+				centLabel.setText(model.getLikeliestState());
+		}
+		else if(i == 4)
+		{
+				statNumber = 4;
+				topLabel.setText("Top 10 UFO Recent Sights Playlist");
+				centLabel.setText("");
+		}
+	}
+	
+	public int getStat()
+	{
+		return statNumber;
+	}
+	
+	// Add actionlistener to button. Set stat (currentStatnumber +1).
+	// Within setstat, compare to the values inside a static arraylist. if it is contained, increase. etc,etc. Then pass to current setstat method when found available stat.
+	
 
 }
