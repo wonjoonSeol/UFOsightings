28228d37ab132ca72012db62acf024509e56b527
Munkhtulga Battogtokh <munkhtulga.battogtokh@kcl.ac.uk>
2017-03-30 14:23:49 +0100

    Re-optimised StateUS, made images relatively positioned to panel

--- ppa-major-coursework/src/assessment/model/panel2/StateUS.java
+++ ppa-major-coursework/src/assessment/model/panel2/StateUS.java
@@ -12,13 +12,11 @@ public class StateUS extends Observable {
 	private String abbreviation;			// state abbreviation, eg. ARZ
 	private ArrayList<Incident> incidents; 	// list of incidents 
 	private int incidentsCount; 			// count of incidents 
-	private TreeMap<Integer, Integer> mapYearToCount; // map years to incident counts that year
 	
 	public StateUS(String name, String abbreviation) {
 		this.name = name; 
 		this.abbreviation = abbreviation; 
 		this.incidents = new ArrayList<Incident>(); 
-		this.mapYearToCount = new TreeMap<>(); 
 	}
 	
 	/** 
@@ -28,15 +26,6 @@ public class StateUS extends Observable {
 	public void addIncident(Incident incident) {
 		incidents.add(incident); 
 		incidentsCount++; 
-		
-		int incidentYear = Integer.parseInt(incident.getDateAndTime().substring(0, 4)); 
-	
-		if (mapYearToCount.containsKey(incidentYear)) {
-			mapYearToCount.put(incidentYear, mapYearToCount.get(incidentYear) + 1); 
-		} else {
-			mapYearToCount.put(incidentYear, 1); 
-		}	
-		
 		
 		setChanged(); 
 		notifyObservers(); 
@@ -60,7 +49,6 @@ public class StateUS extends Observable {
 	public void clearIncidents() {
 		incidents.clear();
 		incidentsCount = 0; 
-		mapYearToCount.clear();
 
 		setChanged(); 
 		notifyObservers(); 
@@ -71,47 +59,24 @@ public class StateUS extends Observable {
 	}
 	
 	/** 
-	 * Gives the map that maps incident years to number of incidents that year
+	 * Gives map that maps incident years to number of incidents that year
 	 * @return TreeMap<Integer, Integer> map of incident years to incident count that year
 	 */
 	public TreeMap<Integer, Integer> getIncidentPerYear() {
+		
+		TreeMap<Integer, Integer> mapYearToCount = new TreeMap<>(); 
+		
+		for (Incident incident : incidents) {
+			int incidentYear = Integer.parseInt(incident.getDateAndTime().substring(0, 4)); 
+		
+			if (mapYearToCount.containsKey(incidentYear)) {
+				mapYearToCount.put(incidentYear, mapYearToCount.get(incidentYear) + 1); 
+			} else {
+				mapYearToCount.put(incidentYear, 1); 
+			}	
+		}
+		
 		return mapYearToCount; 
 	}
 	
-	/** 
-	 * Gives the current highest count of incidents that occured in any of the years. 
-	 * @return
-	 */
-	public int maxIncidentCountAnyYear() {
-		int count = 0; 
-		for (int i : mapYearToCount.values()) {
-			if (i > count) {
-				count = i; 
-			}
-		}
-		return count; 
-	}
-	
-	/** 
-	 * Gives all the years that the State's current incidents happen in 
-	 * @return years Integer[] years that have incidents
-	 */
-	public Integer[] years() {
-	
-		Integer[] years = new Integer[ mapYearToCount.keySet().size()];
-		int index = 0; 
-		for (int i : mapYearToCount.keySet()) {
-			years[index++] = i; 
-		}
-	
-		return years; 
-	}
-	
-	/** 
-	 * Gives how many different years there are that have incidents currently within this State
-	 * @return int how many different years with incidents
-	 */
-	public int countYears() {
-		return mapYearToCount.keySet().size();  
-	}
 }
